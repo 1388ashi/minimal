837b696255395ce4abd883ef20025a5d34ae3b4a
1388ashi <143864702+1388ashi@users.noreply.github.com>
2024-06-15 13:58:07 +0330

    Update api.php

--- Modules/Product/routes/api.php
+++ Modules/Product/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Product\Http\Controllers\Api\ProductController;
+use Modules\Product\Http\Controllers\api\ProductController;
 
 /*
  *--------------------------------------------------------------------------
