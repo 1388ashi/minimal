660a3eee6c1f37f7bcb4e56fdf92402815ec2762
1388ashi <arshiabat5@gmail.com>
2024-06-19 14:20:30 +0430

    fix panel admin

--- Modules/JobOffer/app/Models/JobOffer.php
+++ Modules/JobOffer/app/Models/JobOffer.php
@@ -2,9 +2,7 @@
 
 namespace Modules\JobOffer\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Modules\Core\Models\BaseModel;
 use Spatie\Activitylog\LogOptions;
