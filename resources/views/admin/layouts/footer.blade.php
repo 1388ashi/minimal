4b30b181284c77115125b79828e03a305096e58a
1388ashi <143864702+1388ashi@users.noreply.github.com>
2024-06-20 13:27:52 +0330

    Update footer.blade.php

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,1 +1,1 @@
-<footer class="footer"> <div class="container"> <div class="row align-items-center flex-row-reverse"> <div class="col-md-12 col-sm-12 mt-3 mt-lg-0 text-center"> طراحی و توسعه توسط <b> شتاب.</b>  
+<footer class="footer"> <div class="container"> <div class="row align-items-center flex-row-reverse"> <div class="col-md-12 col-sm-12 mt-3 mt-lg-0 text-center"> طراحی و توسعه توسط <b>گروه شتاب.</b>  
