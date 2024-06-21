18f329fb6b94c4c5647443f2d006443f481b0a77
1388ashi <arshiabat5@gmail.com>
2024-06-21 14:25:22 +0430

    fix api show post

--- Modules/Blog/app/Http/Controllers/api/PostsController.php
+++ Modules/Blog/app/Http/Controllers/api/PostsController.php
@@ -59,7 +59,7 @@ class PostsController extends Controller
         $viewCount = views($post)->count();
         $mostViewedPosts = Post::orderByViews()->take(4)->get();
         $categories = BlogCategory::select('id','title')->with('posts:id,title,summary,body,featured,created_at')->where('type',$post->type)->teke(5)->get();
-        $morePosts = Post::select('id','title','writer','read','type','category_id','body','summary','created_at')->where('category_id',$post->category_id)->get(8);
+        $morePosts = Post::select('id','title','writer','read','type','category_id','body','summary','created_at')->where('category_id',$post->category_id)->take(8)->get();
 
         return response()->success("مشخصات بلاگ {$post->id}",compact('post','searchPost','viewCount','mostViewedPosts','categories'));
     }
