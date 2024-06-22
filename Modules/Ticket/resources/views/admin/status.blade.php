45cf1a5004c826d1688fcdacd9f44558be6144bf
Mahmoud <mahmoudgilak88@yahoo.com>
2024-06-22 23:49:03 +0430

    model ticket

--- Modules/Ticket/resources/views/admin/status.blade.php
+++ Modules/Ticket/resources/views/admin/status.blade.php
@@ -1,5 +1,5 @@
 @if($ticket->status == 'pending')
-    <span title="وضعیت" class="badge badge-warning ">خوانده نشده</span>
+    <span title="وضعیت" class="badge badge-danger ">خوانده نشده</span>
 @elseif($ticket->status == 'accepted')
     <span title="وضعیت" class="badge badge-success ">خوانده شده</span>
 @endif
