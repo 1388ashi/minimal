b6c3ec84ebb7bebd75a9195f8e11f2e55198913c
arshia <arshiabat5@gmail.com>
2024-07-01 01:31:34 +0430

    api work samples

--- Modules/WorkSample/app/Http/Controllers/Api/WorkSampleController.php
+++ Modules/WorkSample/app/Http/Controllers/Api/WorkSampleController.php
@@ -3,6 +3,7 @@
 namespace Modules\WorkSample\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Modules\WorkSample\Models\WorkSample;
 
 class WorkSampleController extends Controller
@@ -10,7 +11,7 @@ class WorkSampleController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
         $workSamples = WorkSample::select('id','title')->latest('id')->get();
 
