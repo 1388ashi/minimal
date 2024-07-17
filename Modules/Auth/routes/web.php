eb9052b35127375eee729a497fb1e209f458487d
1388ashi <143864702+1388ashi@users.noreply.github.com>
2024-07-17 09:47:27 +0330

    Update web.php

--- Modules/Auth/routes/web.php
+++ Modules/Auth/routes/web.php
@@ -16,7 +16,7 @@ use Modules\Auth\Http\Controllers\Admin\AuthController;
 
     //auth
     Route::get('/', [AuthController::class, 'showLoginForm'])->name('login.form');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/', [AuthController::class, 'login'])->name('login');
 
     Route::middleware('auth')->name('admin.')->prefix('admin')->group(function () {
         //auth
