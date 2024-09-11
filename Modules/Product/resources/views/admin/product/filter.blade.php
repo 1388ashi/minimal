4dc3b6f242f1eb065e17e15880ca855a73de1b0c
arshia <arshiabat5@gmail.com>
2024-09-11 10:45:26 +0430

    modify filter order and create shipping filter

--- Modules/Product/resources/views/admin/product/filter.blade.php
+++ Modules/Product/resources/views/admin/product/filter.blade.php
@@ -13,30 +13,36 @@
     <div class="card-body">
             <form action="{{ route("admin.products.index") }}">
                 <div class="row">
-                    <div class="col-3 col-lg-3 form-group">
-                        <label class="font-weight-bold">عنوان:</label>
-                        <input type="text" name="title" value="{{ request("title") }}" placeholder="عنوان را انتخاب کنید" class="form-control" />
+                    <div class="col-12 col-md-6 col-xl-3">
+                        <div class="form-group">
+                            <label>عنوان:</label>
+                            <input type="text" name="title" value="{{ request("title") }}" placeholder="عنوان را انتخاب کنید" class="form-control" />
+                        </div>
                     </div>
-                    <div class="col-3 col-lg-3 form-group">
-                        <label class="font-weight-bold">دسته بندی :</label>
-                        <select name="category_id" class="form-control select2">
-                            <option value="">همه</option>
-                            @foreach ($categories as $category)
-                                <option value="{{ $category->id }}" @selected(request("category_id") == $category->id)>{{ $category->title }}</option>
-                            @endforeach
-                        </select>
+                    <div class="col-12 col-md-6 col-xl-3">
+                        <div class="form-group">
+                            <label>دسته بندی :</label>
+                            <select name="category_id" class="form-control select2">
+                                <option value="">همه</option>
+                                @foreach ($categories as $category)
+                                    <option value="{{ $category->id }}" @selected(request("category_id") == $category->id)>{{ $category->title }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                    <div class="col-3 col-lg-3 form-group">
-                            <label class="font-weight-bold">تخفیف :</label>
+                    <div class="col-12 col-md-6 col-xl-3">
+                        <div class="form-group">
+                            <label>تخفیف :</label>
                             <select name="discount" class="form-control">
                                 <option value="">همه</option>
                                 <option value="1" @selected(request("discount") == "1")>دارد</option>
                                 <option value="0" @selected(request("discount") == "0")>ندارد</option>
                             </select>
+                        </div>
                     </div>
-                    <div class="col-3 col-lg-3 form-group">
+                    <div class="col-12 col-md-6 col-xl-3">
                         <div class="form-group">
-                            <label class="font-weight-bold">وضعیت :</label>
+                            <label>وضعیت :</label>
                             <select name="status" class="form-control">
                                 <option value="">همه</option>
                                 <option value="0" @selected(request("status") == "0")>غیر فعال</option>
@@ -46,16 +52,18 @@
                     </div>
                 </div>
 
-                    <div class="row">
-                        <div class="col-9">
-                            <button class="col-12 btn btn-primary align-self-center">جستجو</button>
-                        </div>
-                        <div class="col-3">
-                            <a href="{{route('admin.products.index')}}" class="col-12 btn btn-danger align-self-center">حذف فیلتر ها<i
-                                class="fa fa-close"
-                                aria-hidden="true"></i></a>
-                        </div>
+                <div class="row">
+
+                    <div class="col-12 col-md-6 col-xl-9">
+                      <button class="btn btn-primary btn-block" type="submit">جستجو <i class="fa fa-search"></i></button>
                     </div>
+
+                    <div class="col-12 col-md-6 col-xl-3">
+                      <a href="{{ route('admin.products.index') }}"
+                         class="btn btn-danger btn-block">حذف همه فیلتر ها <i class="fa fa-close"></i></a>
+                    </div>
+
+                  </div>
                 </div>
             </form>
     </div>
