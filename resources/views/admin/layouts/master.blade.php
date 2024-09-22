65320239cf0a44002fd98f98fc31915d092dcdab
1388ashi <143864702+1388ashi@users.noreply.github.com>
2024-09-22 14:28:25 +0330

    Update master.blade.php

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -109,6 +109,7 @@
 
         <!-- Select2 js -->
         <script src="{{asset('assets/plugins/select2/select2.full.min.js')}}"></script>
+        <script src="{{asset('assets/plugins/sweet-alert/sweetalert.min.js')}}"></script>
 
 {{--        <!-- INTERNAL Index js-->--}}
 {{--        <script src="{{asset('assets/js/index1.js')}}"></script>--}}
