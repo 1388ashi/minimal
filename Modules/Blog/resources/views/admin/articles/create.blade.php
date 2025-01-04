ac31ec48807f3d4fde737e78c2a30b8607a5fb80
1388ashi <arshiabat5@gmail.com>
2025-01-04 11:49:17 +0330

    .

--- Modules/Blog/resources/views/admin/articles/create.blade.php
+++ Modules/Blog/resources/views/admin/articles/create.blade.php
@@ -73,7 +73,6 @@
                         <div class="row">
                             <label >توضیحات</label><span class="text-danger">&starf;</span>
                             <textarea name="description" class="form-control ckeditor" cols="100" rows="4">{{old('description')}}</textarea>
-                            <textarea name="body"  class="form-control ckeditor cols="100" rows="4">{{old('body')}}</textarea>
                         </div>
                         <div class="row mt-2">
                             <div class="col-md-4">
