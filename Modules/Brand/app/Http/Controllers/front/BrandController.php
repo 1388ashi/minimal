5b935115244d9a85d3d18afac2264f20f9adcc94
1388ashi <arshiabat5@gmail.com>
2025-01-04 12:49:42 +0330

    .

--- Modules/Brand/app/Http/Controllers/front/BrandController.php
+++ Modules/Brand/app/Http/Controllers/front/BrandController.php
@@ -15,7 +15,7 @@ class BrandController extends Controller
         $brands = Brand::select('id','title','status','description','category_id')->get();
         $categories = Category::whereIn('id', Brand::pluck('category_id'))->get();  
         
-        return response()->success('',compact('brands'));
+        return response()->success('',compact('brands','categories'));
     }
     public function show(Brand $brand): mixed  
     {  
