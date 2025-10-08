d8d2443f617c756afcebf0b50a13f78fc83f4af3
arshia88 <arshiabat5@gmail.com>
2025-10-08 15:13:08 +0330

    modify some buges

--- Modules/Brand/app/Http/Controllers/Front/BrandController.php
+++ Modules/Brand/app/Http/Controllers/Front/BrandController.php
@@ -12,13 +12,17 @@ class BrandController extends Controller
 {
     public function index()
     {
-       $brands = Brand::select('id','title','status','description','category_id')->get();
-       $categories = Category::whereIn('id', Brand::pluck('category_id'))->get();
-        
+        $brands = Brand::select('brands.id', 'brands.title', 'brands.status', 'brands.description','brands.slug')  
+            ->with('categories:id,title')->orderBy('order', 'asc')->get()->makeHidden(['background']);  
+
+        $categoryIds = $brands->pluck('categories.*.id')->flatten()->unique();
+        $categories = Category::whereIn('id', $categoryIds)->get();
+
         return response()->success('',compact('brands','categories'));
     }
-    public function show(Brand $brand): mixed  
+    public function show($slug): mixed  
     {  
+        $brand = Brand::where('slug',$slug)->firstOrFail();
         $moreBrands = Brand::where('id', '!=', $brand->id)->get();  
         $products = Product::where('brand_id',$brand->id)->with('categories:id,title')->take(4)->get();
         if (!$products) {
